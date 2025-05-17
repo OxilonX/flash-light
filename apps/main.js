fb71291136fd7a8dda7ed0d75cbb56488f0c4645
oxilon <boabmeuni01@gmail.com>
2025-05-17 15:30:06 +0100

    flash light done

---
+++ apps/main.js
@@ -0,0 +1,17 @@
+const button = document.querySelector(".js-flash-button");
+const rotation = document.querySelector(".image-light-container");
+const openLight = document.querySelector(".light");
+let On = 0;
+button.onclick = () => {
+  let i;
+  rotation.classList.toggle("try-open-light");
+  i = (Math.random() * 10).toFixed();
+  if (On === 0 && i < 4) {
+    On = 1;
+    rotation.classList.remove("try-open-light");
+    openLight.classList.add("open-light");
+  } else {
+    On = 0;
+    openLight.classList.remove("open-light");
+  }
+};
